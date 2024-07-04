70b5520267c82519710eb0a8702b16f9823d5257
Sadia-H <sadiahussain118@gmail.com>
2024-07-04 12:14:36 +0100

    Non Painted Walls

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,28 +6,72 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        int numberOfWalls = ValidInputs.getValidNumber(s, "How many walls are in the room?");
+        int numberOfWalls = getValidNumber(s, "How many walls are in the room?");
 
         double totalArea = 0;
 
         for (int i = 1; i <= numberOfWalls; i++) {
-            int wallHeight = ValidInputs.getValidNumber(s, "What is the height of the wall " + i + " in metres?");
-            int wallWidth = ValidInputs.getValidNumber(s, "What is the width of the wall " + i + " in metres?");
-            int wallArea = 0;
+            double wallHeight = getValidDouble(s, "What is the height of the wall " + i + " in metres?");
+            double wallWidth = getValidDouble(s, "What is the width of the wall " + i + " in metres?");
+            double wallArea = 0;
 
             wallArea = wallHeight * wallWidth;
             System.out.println("The area of the wall is: " + wallArea + " metres.");
             totalArea += wallArea;
 
         }
+
+        int numberOfNonPaintedWalls = getValidNumber(s, "How many walls/areas will not be painted? (If none, enter 0.)");
+        double wallAreaNotPainted = 0;
+
+        for (int i =1; i<= numberOfNonPaintedWalls; i++) {
+            double noPaintWalls = getValidDouble(s, "Please enter the area of wall: " + i + " in metres." );
+            wallAreaNotPainted += noPaintWalls;
+        }
+
+        totalArea = totalArea - wallAreaNotPainted;
         System.out.println("The total area of the room is: " + totalArea + " metres squared.");
 
-        double paintCoverage = ValidInputs.getValidDouble(s, "What is the paint coverage per litre (in square feet)?");
+        double paintCoverage = getValidDouble(s, "What is the paint coverage per litre (in square feet)?");
 
         double paintRequired = totalArea / paintCoverage;
         System.out.println("The amount of paint required for this room is: " + paintRequired + "L.");
 
 
+    }
+
+    public static int getValidNumber(Scanner scanner, String prompt) {
+        int validInput = -1;
+        while (validInput <= 0) {
+            System.out.println(prompt);
+            if (scanner.hasNextInt()) {
+                validInput = scanner.nextInt();
+                if (validInput <= 0) {
+                    System.out.println("Please enter a number greater than 0.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.next();
+            }
+        }
+        return validInput;
+    }
+
+    public static double getValidDouble(Scanner scanner, String prompt) {
+        int validDouble = -1;
+        while (validDouble <= 0) {
+            System.out.println(prompt);
+            if (scanner.hasNextInt()) {
+                validDouble = scanner.nextInt();
+                if (validDouble <= 0) {
+                    System.out.println("Please enter a number greater than 0.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.next();
+            }
+        }
+        return validDouble;
     }
 
 
